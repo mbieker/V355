12dcddb76c1b61a1999f55c36d4577881b172644
Julian Surmann <julian.surmann@udo.edu>
2014-01-15 19:10:43 +0100

    gj

--- auswertung355.py
+++ auswertung355.py
@@ -87,11 +87,14 @@ def lin_reg(x,y):
 "###########################################################"
 
 "AUFGABENTEIL A"
+"aCk=Werte von Ck in Teil a, Cka=Werte in korrekter Größenordnung für Teil a"
+"bcCk=Werte von Ck in Teil b und c, Ck=in korrekter Größenordnung"
+"aVerhaeltnis: Verhältnis von kleiner zu großer Welle"
 "Experimentelle Werte:"
-aCk=array([2.03,3.00,4.00,5.02,6.47,8.00,9.99])
+aCk=array([ufloat(2.03,0.0609),ufloat(3.00,0.09),ufloat(4.00,0.12),ufloat(5.02,0.1506),ufloat(6.47,0.1941),ufloat(8.00,0.24),ufloat(9.99,0.2997)])
 aVerhaeltnis=array([ufloat(3.5,0.5),ufloat(4.5,0.5),ufloat(6.5,0.5),ufloat(8,0.5),ufloat(9.75,0.5),ufloat(11,0.5),ufloat(13,0.5)])
 Cka = aCk / 1000000000
-bcCk=array([2.03,3.00,4.00,5.02,6.47,8.00,9.99])
+bcCk=array([ufloat(1.01,0.0303),ufloat(2.03,0.0609),ufloat(3.00,0.09),ufloat(4.00,0.12),ufloat(5.02,0.1506),ufloat(6.47,0.1941),ufloat(8.00,0.24),ufloat(9.99,0.2997)])
 Ck=bcCk/1000000000
 "Theoretische Werte berechnen:"
 L = ufloat(0.032351, 0.00005)
@@ -109,6 +112,7 @@ print "nueMinus:"
 nueMinus=1/(2*math.pi*(L*((1/C)+(2/Cka))**(-1)+L*Csp)**0.5)
 print nueMinus
 
+"nueMittel ist der Mittelwert der beiden Nüs"
 print "nueMittel:"
 nueMittel = (0.5*(nuePlus+nueMinus))
 print nueMittel
@@ -125,6 +129,9 @@ print "abweichungen in Prozent:"
 abweichungen = abs(aVerhaeltnis/verhaeltnis*100-100)
 print abweichungen
 
+tab1 = array([Cka,aVerhaeltnis,nueMinus,nueMittel,schwebung,verhaeltnis,abweichungen])
+print make_LaTeX_table(tab1.T, [r'Ck',r'verhaeltnis',r'nieMinustheo',r'nueMittel',r'schwebung',r'verhältnis',r'abweichungen'])
+
 "###########################################################"
 
 "AUFGABENTEIL B"
@@ -135,8 +142,10 @@ print nuePlus
 print "nueMinus"
 print nueMinus
 "Experimentell bestimmte Werte:"
+"exnuePlus/Minus= gemessene Werte"
 exnuePlus=array([30490,30490,30490,30490,30490,30490,30490])
 exnueMinus=array([40010,37300,35760,34780,33880,33280,33750])
+"Hier kommt der Vergleich mit den Theoriewerten aus a"
 abwPlusTeilB=abs(exnuePlus/nuePlus)
 abwMinusTeilB=abs(exnueMinus/nueMinus)
 
@@ -147,6 +156,8 @@ print abwPlusTeilB
 print "Abweichung in Prozent von Nue-"
 print abwMinusTeilB
 
+tab2 = array([nueMinus,exnuePlus,exnueMinus,abwPlusTeilB,abwMinusTeilB])
+print make_LaTeX_table(tab2.T, [r'nueminus',r'explus',r'exminus',r'abwplus',r'abwminus'])
 "###########################################################"
 
 "AUFGABENTEIL C"
@@ -154,37 +165,33 @@ print abwMinusTeilB
 time=21
 flow=3800
 fhigh=74200
-peak1time=9
-peak2time=array([13.7,11.8,10.9,10.5,10.1,9.9,9.5,9.5])
-peak1=array([1.22,1.25,1.25,1.3,1.33,1.4,1.46,1.55])
-peak2=array([0.9,1.02,1.1,1.15,1.19,1.3,1.34,1.43])
+peak1time=ufloat(9,0.5)
+peak2time=array([ufloat(13.7,0.5),ufloat(11.8,0.5),ufloat(10.9,0.5),ufloat(10.5,0.5),ufloat(10.1,0.5),ufloat(9.9,0.5),ufloat(9.5,0.5),ufloat(9.5,0.5)])
 
 print "Aufgabenteil C:"
 "Peakzeiten auf Frequenzen umrechnen:"
-peak1freq=(70400*9/21)+3800
+peak1freq=(70400*peak1time/21)+3800
 peak2freq=(70400*peak2time/21) +3800
 print "peak1freq"
 print peak1freq
 print "peak2freq:"
 print peak2freq
 peak1=array([ufloat(1.22,0.05),ufloat(1.25,0.05),ufloat(1.25,0.05),ufloat(1.3,0.05),ufloat(1.33,0.05),ufloat(1.4,0.05),ufloat(1.46,0.05),ufloat(1.55,0.05)])
-peak2=array([ufloat(0.5,0.05),ufloat(1.02,0.05),ufloat(1.1,0.05),ufloat(1.15,0.05),ufloat(1.19,0.05),ufloat(1.3,0.05),ufloat(1.34,0.05),ufloat(1.43,0.05)])
-I1=(peak1/48)
-I2=(peak2/48)
+peak2=array([ufloat(0.9,0.05),ufloat(1.02,0.05),ufloat(1.1,0.05),ufloat(1.15,0.05),ufloat(1.19,0.05),ufloat(1.3,0.05),ufloat(1.34,0.05),ufloat(1.43,0.05)])
+I1=(peak1/73)
+I2=(peak2/73)
 print "I1:"
 print I1
 print "I2:"
 print I2
+
+
  
 Cke = 2.03
 
 def Lbetrag(f):
     return((1/(8*math.pi**2*Cke**2*48**2*(2*math.pi*L-1/(2*math.pi*f)*(1/C + 1/Cke))**2+(1/(2*math.pi*f*Cke)-2*math.pi*f*(2*math.pi*L-1/(2*math.pi*f)*(1/C + 1/Cke))**2+2*math.pi*f*48*22*Cke)**2)**0.5))
-    
-f=linspace(20000,50000)
-
-plot(f,[i.n for i in Lbetrag(f)])
-show()
+  
 
 "###########################################################"
 
